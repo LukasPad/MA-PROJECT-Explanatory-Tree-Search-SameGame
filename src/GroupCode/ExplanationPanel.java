b33492f994ad9d6afa4e6aeb62ff76f53696c88a
AlmightyPeanut <benedikthornig@outlook.com>
2022-06-20 10:03:01 +0200

    todos for explanations

--- src/GroupCode/ExplanationPanel.java
+++ src/GroupCode/ExplanationPanel.java
@@ -32,6 +32,15 @@ public class ExplanationPanel extends JTextArea {
     }
 
     public void updateExplanation(int boardX, int boardY) {
+        // TODO (E): aggregate cluster to one explanation
+        // TODO (B): feature collector -> run only once per mcts move (either via boardBanel.humanPlay or computerPlayThread or var in explanationPanel)
+        // TODO (B): give feature representations: for tree features just iterate with findGameFeatures over all nodes
+        // TODO: extract specific features from feature collector and filter by relevance based on the occurrences in the other nodes (and then compare them either to best move or selected move)
+        // TODO: take mcts score into consideration for explanations
+        // TODO: compare the extracted features to the lookup table aka. results of data analysis
+        // TODO: give explanation based on analysis and extracted features
+
+        // (relevance ranking => score) or (feature rules) => scoring/bad/tactical move
         explanation = getExplanation(boardX, boardY);
         this.repaint();
     }
